95637465002c0ddb23d1cdfd0b8f4c4b50b10fb3
julia-azevedo <jap@ic.ufal.br>
2024-11-05 11:40:07 -0300

    add receive message and main functions

--- client.py
+++ client.py
@@ -21,3 +21,69 @@ def get_nickname():
 
     return nickname
 
+def receive_message(client_socket, window, nickname):
+    while True:
+        try:
+            data = client_socket.recv(10240)
+
+            if not data:
+                break
+
+            msg = data.decode('utf-8')
+            print('Mensagem recebida:', msg)
+
+            if f'{nickname} saiu' in msg:
+                client_socket.close()
+                print('Fechando socket')
+                break
+
+            output = data.decode('utf-8') + '/n'
+            window['-OUTPUT-'].update(output, appende=True)
+        except Exception as e:
+            break
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 65432
+
+if __name__ == '__main__':
+    nickname = get_nickname()
+    
+     layout = [[sg.Text('Usuário: ' + nickname)],
+            [sg.Multiline(size=(50, 20), key='-OUTPUT-', disabled=True)],
+            [sg.Input(size=(50, 1), key='-INPUT-')],
+            [sg.Button('Enviar'), sg.Button('Sair')]]
+
+    window = sg.Window('ChatRoom', layout)
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+    print(f"Conectando a {SERVER_HOST}:{SERVER_PORT}")
+
+    enter_message = f'ENTER {nickname} entrou na sala!'
+    client.socket.send(enter_message.encode('utf-8'))
+    client.socket.recv(10240)
+
+    send_thread = threading.Thread(target=receive_message, args=(client_socket, window, nickname))
+    send_thread.start()
+
+    while True:
+        event, values = window.read()
+
+        if event in (None, 'Sair'):
+            exit_message = f'EXIT {nickname} saiu da sala!'
+            client_socket.send(exit_message.encode('utf-8'))
+
+            while send_thread.is_alive():
+                pass
+
+            break
+        
+        if event == 'Enviar':
+            message = values['-INPUT-']
+            window['-INPUT-'].update('')
+            message = nickname + ' ' + message
+            client_socket.send(message.encode('utf-8'))
+
+    window.close()
+
